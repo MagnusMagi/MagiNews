af81b84c96f7487dcf1c72a39ed279087ac214bf
MagnusMagi <magnusmagi.temp@gmail.com>
2025-08-24 00:15:40 +0300

    Initial Commit

---
+++ MagiNewsIOSTests/MagiNewsIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MagiNewsIOSTests.swift
+//  MagiNewsIOSTests
+//
+//  Created by Magnus Magi on 24.08.2025.
+//
+
+import Testing
+@testable import MagiNewsIOS
+
+struct MagiNewsIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
